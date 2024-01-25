c651e9abf80e7829539ea73624fefa40a9666ec2
Harsh <hk3711750@gmail.com>
2024-01-26 01:37:54 +0530

    Changes

--- scroll.js
+++ scroll.js
@@ -1,31 +1,3 @@
-// var cur = 0;
-// var target = 2500;
-// var scroll = setInterval(function (){
-//     if(cur>=target){
-//         clearInterval(scroll);
-//         return;
-//     }
-//     cur += 50;
-//     scrollBy(0,50);
-// },50);
-
-// var section = document.getElementsByTagName("section");
-
-// for(var i = 0 ; i<section.length ; i++){
-//     section[i].addEventListener('click',function(event){
-//         event.preventDefault();
-//         var target = document.getElementById("event");
-//         var coordinates = target.getBoundingClient();
-//         var scroll = setInterval(function(){
-//             if(coordinates.top == 0){
-//                 clearInterval(scroll);
-//                 return;
-//             }
-//             scrollBy(0,50);
-//         },50);
-//     });
-// }
-
 var navmenutag = document.querySelectorAll(".nav-menu a");
 var interval;
 for(var i=0 ; i<navmenutag.length ; i++){
